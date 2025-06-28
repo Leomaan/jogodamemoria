f71dab010ae1798b4ccb6e6b566497d97ed2a43b
cauanegaldino <cauanegaldino@gmail.com>
2025-06-28 14:47:05 -0300

    Novas telas e adaptações

---
+++ core/src/Telas/MenuScreen.java
@@ -0,0 +1,84 @@
+package Telas;
+
+import br.com.leoman.jogodamemoria.JogoDaMemoria;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class MenuScreen implements Screen {
+    private final JogoDaMemoria game;
+    private final Stage stage;
+
+    public MenuScreen(JogoDaMemoria game) {
+        this.game = game;
+        this.stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        TextButton buttonJogar = new TextButton("Jogar", skin);
+        TextButton buttonSair = new TextButton("Sair", skin);
+
+        table.add(buttonJogar).pad(10).row();
+        table.add(buttonSair).pad(10).row();
+
+        buttonJogar.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new JogoScreen(game));
+            }
+        });
+
+
+        buttonSair.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+    }
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.8f, 0.8f, 0.8f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
