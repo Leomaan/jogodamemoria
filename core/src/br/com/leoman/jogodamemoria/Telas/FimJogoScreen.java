2ac449ee27a99e4723f84c6c56fed33fb288c006
Leomaan <146137607+Leomaan@users.noreply.github.com>
2025-07-09 21:54:55 -0300

    mudanças na tela de fim

--- core/src/br/com/leoman/jogodamemoria/Telas/FimJogoScreen.java
+++ core/src/br/com/leoman/jogodamemoria/Telas/FimJogoScreen.java
@@ -4,12 +4,10 @@ import br.com.leoman.jogodamemoria.JogoDaMemoria;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
@@ -24,18 +22,31 @@ public class FimJogoScreen implements Screen {
         this.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-        Skin skin = new Skin(Gdx.files.internal("uiskin.json")); // certifique-se que está no assets/
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
 
         Table table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
 
-        String mensagem = venceu ? "Você venceu!" : "Você perdeu!";
-        Label labelResultado = new Label(mensagem, skin, "default");
+        Texture texturaVenceu = new Texture(Gdx.files.internal("assets/tituloVenceu.png"));
+        Image imagemVenceu = new Image(texturaVenceu);
+
+        Texture texturaPerdeu = new Texture(Gdx.files.internal("assets/tituloPerdeu.png"));
+        Image imagemPerdeu = new Image(texturaPerdeu);
+
+
+
+
+        if (venceu) {
+            table.add(imagemVenceu).width(633).height(394).padBottom(100).row();
+        } else {
+            table.add(imagemPerdeu).width(633).height(394).padBottom(100).row();
+        }
+
         TextButton botaoReiniciar = new TextButton("Voltar ao Menu", skin);
 
-        table.add(labelResultado).pad(20).row();
-        table.add(botaoReiniciar).pad(20).row();
+        table.add(botaoReiniciar).width(150).height(80).pad(20).row();
+        botaoReiniciar.setColor(0.8588f, 0.8196f, 0.3215f, 1f);
 
         botaoReiniciar.addListener(new ClickListener() {
             @Override
@@ -50,7 +61,7 @@ public class FimJogoScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.9f, 0.9f, 0.9f, 1f);
+        Gdx.gl.glClearColor(0.8588f, 0.8196f, 0.3215f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act(delta);
